13f19c1e2e6eb7f49fa09bedff03ac1bfd4f4bff
Sam Kim <seungkim11@gmail.com>
2016-03-18 15:11:06 -0700

    2014

--- data_extractor/src/main/java/BusinessParser.java
+++ data_extractor/src/main/java/BusinessParser.java
@@ -37,7 +37,7 @@ public class BusinessParser {
                 if (line.startsWith("MV")) {
                     if (!line.contains("TV")) {
                         year = parseYear(line);
-                        if (year >= 2015) {
+                        if (year >= 2014) {
                             hasMV = true;
                             title = parseTitle(line);
                         }
